bacf69ea7d6f11ea7acb35033ddb6f8eb0eaee5b
liangxin1300 <XLiang@suse.com>
2018-04-17 14:12:25 +0800

    test2

--- srv/salt/_runners/select.py
+++ srv/salt/_runners/select.py
@@ -1,7 +1,20 @@
 # -*- coding: utf-8 -*-
 
+import logging
 import minions
+import salt.client
+
+log = logging.getLogger(__name__)
+
 
 def all_minions():
     target = minions.Minions()
     return target.matches
+
+
+def ha_nodes():
+    local = salt.client.LocalClient()
+    target = minions.Minions()
+    for minion in target.matches:
+        print(minion)
+        #print(local.cmd(minion, 'cmd.run', ['crm_node -l']))
