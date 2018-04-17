adecff6b7e58458b7d53f6cd1c024dd0530dd429
liangxin1300 <XLiang@suse.com>
2018-04-17 15:33:57 +0800

    test1

--- srv/salt/_modules/baseinfo.py
+++ srv/salt/_modules/baseinfo.py
@@ -1,4 +1,22 @@
+# -*- coding: utf-8 -*-
+
+import salt.utils.path
 
 
-def test():
-    return True
+class Node(object):
+
+    def __init__(self):
+        self.members = self.get_member()
+        self.cluster_name = self.get_cluster_name()
+
+    def is_ha_node(self):
+        pass
+
+    def is_live_node(self):
+        pass
+
+    def get_member(self):
+        pass
+
+    def get_cluster_name(self):
+        pass
