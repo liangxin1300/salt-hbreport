4650a852454a0598a71d07312e83647ea8be2fdf
liangxin1300 <XLiang@suse.com>
2018-04-18 11:06:40 +0800

    adjust 3

--- srv/pillar/report/constants.sls
+++ srv/pillar/report/constants.sls
@@ -1,4 +1,5 @@
 
 get_nodes_cmd: 'crm node server'
+get_cib_cmd: 'cibadmin -Q -l'
 cib_dir: '/var/lib/pacemaker/cib/'
 cib_file: 'cib.xml'
--- srv/salt/_modules/baseinfo.py
+++ srv/salt/_modules/baseinfo.py
@@ -4,16 +4,11 @@ import os
 from salt.utils import which
 
 
-def test():
-    if __utils__['path.which']("ls"):
-        return True
-    return False
-
-
 class Node(object):
 
     def __init__(self):
-        self.cmd = __salt__['pillar.get']('get_nodes_cmd')
+        self.get_nodes_cmd = __salt__['pillar.get']('get_nodes_cmd')
+        self.get_cib_cmd = __salt__['pillar.get']('get_cib_cmd')
 
     def is_ha_node(self):
         if which("pacemakerd") and which("corosync"):
@@ -27,18 +22,27 @@ class Node(object):
 
     def get_member(self):
         if self.is_live_node():
-            return __salt__['cmd.run_all'](self.cmd)
+            return __salt__['cmd.run_all'](self.get_nodes_cmd)['stdout']
         elif self.is_ha_node():
             cib_dir = __salt__['pillar.get']('cib_dir')
             cib_file = __salt__['pillar.get']('cib_file')
             _env = {"CIB_file": os.path.join(cib_dir, cib_file)}
-            return __salt__['cmd.run_all'](self.cmd, env=_env)
+            return __salt__['cmd.run_all'](self.get_nodes_cmd, env=_env)['stdout']
         else:
             return None
 
     def get_cluster_name(self):
-        pass
+        if self.is_live_node():
+            cmd = self.get_cib_cmd + " -o crm_config"
+            return __salt__['cmd.run_all'](cmd)['stdout']
+
 
 def get_member():
     node = Node()
-    return node.get_member()
+    ret = node.get_member()
+    if ret:
+        return ret.split()
+
+def get_cluster_name():
+    node = Node()
+    return node.get_cluster_name()
